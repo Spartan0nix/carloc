cde895ca58cbeb9b5ad740450d3a99ccb4825bc7
Spartan0nix <l.humblot74@gmail.com>
2021-05-23 20:18:20 +0200

    Started rent_step_2

---
+++ src/Controller/Normalizer/CarNormalizer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Normalizer;
+
+use App\Entity\Car;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CarNormalizer extends AbstractController
+{
+
+    public function normalize(Car $car){
+
+        $typeArray = array();
+        foreach($car->getTypeId() as $type){
+            array_push($typeArray, array(
+                'id' => $type->getId(),
+                'type' => $type->getType()
+            ));
+        }
+
+        $optionArray = array();
+        foreach($car->getOptionId() as $option){
+            array_push($optionArray, array(
+                'id' => $option->getId(),
+                'name' => $option->getName(),
+                'description' => $option->getDescription()
+            ));
+        }
+
+        return array(
+            'id' => $car->getId(),
+            'horsepower' => $car->getHorsepower(),
+            'description' => $car->getDescription(),
+            'daily_price' => $car->getDailyPrice(),
+            'release_year' => $car->getReleaseYear(),
+            'fuel' => array(
+                'id' => $car->getFuelId()->getId(),
+                'fuel' => $car->getFuelId()->getFuel()
+            ),
+            'brand' => array(
+                'id' => $car->getBrandId()->getId(),
+                'brand' => $car->getBrandId()->getBrand()
+            ),
+            'modele' => array(
+                'id' => $car->getModeleId()->getId(),
+                'modele' => $car->getModeleId()->getModele()
+            ),
+            'id' => array(
+                'id' => $car->getColorId()->getId(),
+                'color' => $car->getColorId()->getColor()
+            ),
+            'gearbox' => array(
+                'id' => $car->getGearboxId()->getId(),
+                'gearbox' => $car->getGearboxId()->getGearbox()
+            ),
+            'types' => $typeArray,
+            'options' => $optionArray,
+            'office' => array(
+                'id' => $car->getOfficeId()->getId()
+            )
+        );
+
+    }
+}
