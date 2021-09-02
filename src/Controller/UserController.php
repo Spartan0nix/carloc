b5bd472aab61a8433dd321fa9aca38195d373e21
Spartan0nix <l.humblot74@gmail.com>
2021-09-02 22:13:07 +0200

    Feat: Added user_rent_list page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class UserController extends AbstractController
 {
-    #[Route('/change-identity', name:'user_change_identity')]
+    #[Route('/account/change-identity', name:'user_change_identity')]
     public function changeIdentity(Request $request, EntityManagerInterface $em, GuardAuthenticatorHandler $guardAuthenticatorHandler, LoginFormAuthenticator $loginFormAuthenticator){
         $user = $this->getUser();
         if(!$user){
@@ -52,7 +52,7 @@ class UserController extends AbstractController
         return $this->redirectToRoute('auth_account');
     }
 
-    #[Route('/mon-compte/modification-adresse', name:'user_change_address')]
+    #[Route('/account/change-address', name:'user_change_address')]
     public function changeAddress(Request $request, EntityManagerInterface $em): Response {
         $user = $this->getUser();
         if(!$user){
@@ -74,5 +74,41 @@ class UserController extends AbstractController
 
         $this->addFlash('success', 'Informations modifiées avec succès.');
         return $this->redirectToRoute('auth_account');
+    }
+
+    #[Route('/compte/mes-locations', name:'user_rent_list')]
+    public function listUserRent(): Response {
+        
+        $user = $this->getUser();
+        if(!$user){
+            $this->addFlash('error', 'Accès restreint, authentification requise.');
+            return $this->redirectToRoute('auth_login');
+        }
+
+        $rents = $user->getRents();
+        $normalize_rents = [];
+
+        foreach($rents as $rent){
+            array_push($normalize_rents, [
+                'id' => $rent->getId(),
+                'price' => $rent->getPrice(),
+                'pickup_date' => $rent->getPickupDate(),
+                'return_date' => $rent->getReturnDate(),
+                'car' => [
+                    'id' => $rent->getCarId()->getId(),
+                    'brand' => $rent->getCarId()->getBrandId()->getBrand(),
+                    'model' => $rent->getCarId()->getModelId()->getModel(),
+                    'daily_price' => $rent->getCarId()->getDailyPrice(),
+                ],
+                'status' => [
+                    'id' => $rent->getStatusId()->getId(),
+                    'status' => $rent->getStatusId()->getStatus(),
+                ]
+            ]);
+        }
+
+        return $this->render('security/rent_list.html.twig', [
+            'rents' => $normalize_rents
+        ]);
     }
 }
