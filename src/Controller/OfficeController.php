4a98b7c399765dc57cd7eea709494acc3e3aeb58
Spartan0nix <l.humblot74@gmail.com>
2021-11-30 20:23:12 +0100

    Refactored Office and Rent Controllers

--- src/Controller/OfficeController.php
+++ src/Controller/OfficeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OfficeController extends AbstractController
