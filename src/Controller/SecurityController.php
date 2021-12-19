47a25352a6322785717e3be76253aba3566140df
Spartan0nix <l.humblot74@gmail.com>
2021-12-19 23:23:50 +0100

    Updated support for redirect onAuthenticationSuccess

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,9 +19,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/connexion", name="auth_login")
-     */
+    #[Route('/login', name:'auth_login', methods:['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
